f3543ab0ccfbc671d28e5edd571ef2ebe1c15a91
shyamtopiya <shyamtopiya2652@gmail.com>
2022-08-14 17:26:03 +0530

    navbar tabs pages related changes

--- resources/views/company-profile.blade.php
+++ resources/views/company-profile.blade.php
@@ -4,5 +4,149 @@
 
 @section('content')
   @include('layouts.partials.page-header')
-  Company Profile
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-sm-12">
+          <h2>Company Profile</h2>
+          <p>Our industries are experiencing basic challenges. From climate change and geopolitics to the energy market, we are facing new reality. Some people consider them a threat. At Industris, we believe our job is to turn them into opportunities. 
+          <br>That's why we look for new ways to use our expertise in the energy industry, discover new opportunities and promote innovation around the world. Our ambition is to become the world's most efficient industrial solutions and services provider.</p>
+        </div>
+        <div class="col-md-3 col-sm-12 sm-center">
+          <div class="industris-space-sm"></div>
+          <img src="https://via.placeholder.com/458x478.png" alt="">
+        </div>
+        <div class="col-md-3 col-sm-12 sm-center">
+          <div class="industris-space-sm"></div>
+          <img src="https://via.placeholder.com/458x478.png" alt="">
+        </div>
+      </div>
+    </div>
+    <br>
+    <hr>
+    <br>
+    <div class="container">
+      <div class="row">
+          <h2>Infrastructure</h2>
+          <p>Our industries are experiencing basic challenges. From climate change and geopolitics to the energy market, we are facing new reality. Some people consider them a threat. At Industris, we believe our job is to turn them into opportunities. 
+          <br>That's why we look for new ways to use our expertise in the energy industry, discover new opportunities and promote innovation around the world. Our ambition is to become the world's most efficient industrial solutions and services provider.</p>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div id="projects" class="project-grid gallery row ">
+            <div class="project-item events col-lg-3 col-sm-6">
+              <div class="inner">
+                <a href="https://via.placeholder.com/720x841.png" class="image-link imghvr-shutter-out-vert"
+                  title="gallery zoom image">
+                  <img src="https://via.placeholder.com/720x841.png" alt="">
+                </a>
+              </div>
+            </div>
+            <div class="project-item events col-lg-3 col-sm-6">
+              <div class="inner">
+                <a href="https://via.placeholder.com/720x841.png" class="image-link imghvr-shutter-out-vert"
+                  title="gallery zoom image">
+                  <img src="https://via.placeholder.com/720x841.png" alt="">
+                </a>
+              </div>
+            </div>
+            <div class="project-item events col-lg-3 col-sm-6">
+              <div class="inner">
+                <a href="https://via.placeholder.com/720x841.png" class="image-link imghvr-shutter-out-vert"
+                  title="gallery zoom image">
+                  <img src="https://via.placeholder.com/720x841.png" alt="">
+                </a>
+              </div>
+            </div>
+            <div class="project-item events col-lg-3 col-sm-6">
+              <div class="inner">
+                <a href="https://via.placeholder.com/720x841.png" class="image-link imghvr-shutter-out-vert"
+                  title="gallery zoom image">
+                  <img src="https://via.placeholder.com/720x841.png" alt="">
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div id="projects" class="project-grid gallery row ">
+            <div class="project-item events col-lg-3 col-sm-6">
+              <div class="inner">
+                <a href="https://via.placeholder.com/720x841.png" class="image-link imghvr-shutter-out-vert"
+                  title="gallery zoom image">
+                  <img src="https://via.placeholder.com/720x841.png" alt="">
+                </a>
+              </div>
+            </div>
+            <div class="project-item events col-lg-3 col-sm-6">
+              <div class="inner">
+                <a href="https://via.placeholder.com/720x841.png" class="image-link imghvr-shutter-out-vert"
+                  title="gallery zoom image">
+                  <img src="https://via.placeholder.com/720x841.png" alt="">
+                </a>
+              </div>
+            </div>
+            <div class="project-item events col-lg-3 col-sm-6">
+              <div class="inner">
+                <a href="https://via.placeholder.com/720x841.png" class="image-link imghvr-shutter-out-vert"
+                  title="gallery zoom image">
+                  <img src="https://via.placeholder.com/720x841.png" alt="">
+                </a>
+              </div>
+            </div>
+            <div class="project-item events col-lg-3 col-sm-6">
+              <div class="inner">
+                <a href="https://via.placeholder.com/720x841.png" class="image-link imghvr-shutter-out-vert"
+                  title="gallery zoom image">
+                  <img src="https://via.placeholder.com/720x841.png" alt="">
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br>
+    <hr>
+    <br>
+    <div class="container">
+      <div class="row">
+        <h2>Units</h2>
+          <div class="col-md-3 col-sm-12 sm-center">
+            <div class="industris-space-sm"></div>
+            <img src="https://via.placeholder.com/458x478.png" alt="">
+          </div>
+          <div class="col-md-9 col-sm-12">
+            <p>Our industries are experiencing basic challenges. From climate change and geopolitics to the energy market, we are facing new reality. Some people consider them a threat. At Industris, we believe our job is to turn them into opportunities. 
+            <br>That's why we look for new ways to use our expertise in the energy industry, discover new opportunities and promote innovation around the world. Our ambition is to become the world's most efficient industrial solutions and services provider.</p>
+          </div>
+      </div>
+      <br>
+      <div class="row">
+        <div class="col-md-3 col-sm-12 sm-center">
+          <div class="industris-space-sm"></div>
+          <img src="https://via.placeholder.com/458x478.png" alt="">
+        </div>
+        <div class="col-md-9 col-sm-12">
+          <p>Our industries are experiencing basic challenges. From climate change and geopolitics to the energy market, we are facing new reality. Some people consider them a threat. At Industris, we believe our job is to turn them into opportunities. 
+          <br>That's why we look for new ways to use our expertise in the energy industry, discover new opportunities and promote innovation around the world. Our ambition is to become the world's most efficient industrial solutions and services provider.</p>
+        </div>
+      </div>
+      <br>
+      <div class="row">
+        <div class="col-md-3 col-sm-12 sm-center">
+          <div class="industris-space-sm"></div>
+          <img src="https://via.placeholder.com/458x478.png" alt="">
+        </div>
+        <div class="col-md-9 col-sm-12">
+          <p>Our industries are experiencing basic challenges. From climate change and geopolitics to the energy market, we are facing new reality. Some people consider them a threat. At Industris, we believe our job is to turn them into opportunities. 
+          <br>That's why we look for new ways to use our expertise in the energy industry, discover new opportunities and promote innovation around the world. Our ambition is to become the world's most efficient industrial solutions and services provider.</p>
+        </div>
+      </div>
+    </div>
+  </section>
 @endsection
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -3,6 +3,86 @@
 @section('title','Products')
 
 @section('content')
-  @include('layouts.partials.page-header')
-    Products
+@include('layouts.partials.page-header')
+<section class="padding-bottom-medium">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div id="projects" class="project-grid gallery row ">
+          <div class="project-item events col-lg-3 col-sm-6">
+            <div class="inner">
+              <a href="https://via.placeholder.com/720x841.png" class="image-link imghvr-shutter-out-vert"
+                title="gallery zoom image">
+                <img src="https://via.placeholder.com/720x841.png" alt="">
+              </a>
+            </div>
+          </div>
+          <div class="project-item events col-lg-3 col-sm-6">
+            <div class="inner">
+              <a href="https://via.placeholder.com/720x841.png" class="image-link imghvr-shutter-out-vert"
+                title="gallery zoom image">
+                <img src="https://via.placeholder.com/720x841.png" alt="">
+              </a>
+            </div>
+          </div>
+          <div class="project-item events col-lg-3 col-sm-6">
+            <div class="inner">
+              <a href="https://via.placeholder.com/720x841.png" class="image-link imghvr-shutter-out-vert"
+                title="gallery zoom image">
+                <img src="https://via.placeholder.com/720x841.png" alt="">
+              </a>
+            </div>
+          </div>
+          <div class="project-item events col-lg-3 col-sm-6">
+            <div class="inner">
+              <a href="https://via.placeholder.com/720x841.png" class="image-link imghvr-shutter-out-vert"
+                title="gallery zoom image">
+                <img src="https://via.placeholder.com/720x841.png" alt="">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <div id="projects" class="project-grid gallery row ">
+          <div class="project-item events col-lg-3 col-sm-6">
+            <div class="inner">
+              <a href="https://via.placeholder.com/720x841.png" class="image-link imghvr-shutter-out-vert"
+                title="gallery zoom image">
+                <img src="https://via.placeholder.com/720x841.png" alt="">
+              </a>
+            </div>
+          </div>
+          <div class="project-item events col-lg-3 col-sm-6">
+            <div class="inner">
+              <a href="https://via.placeholder.com/720x841.png" class="image-link imghvr-shutter-out-vert"
+                title="gallery zoom image">
+                <img src="https://via.placeholder.com/720x841.png" alt="">
+              </a>
+            </div>
+          </div>
+          <div class="project-item events col-lg-3 col-sm-6">
+            <div class="inner">
+              <a href="https://via.placeholder.com/720x841.png" class="image-link imghvr-shutter-out-vert"
+                title="gallery zoom image">
+                <img src="https://via.placeholder.com/720x841.png" alt="">
+              </a>
+            </div>
+          </div>
+          <div class="project-item events col-lg-3 col-sm-6">
+            <div class="inner">
+              <a href="https://via.placeholder.com/720x841.png" class="image-link imghvr-shutter-out-vert"
+                title="gallery zoom image">
+                <img src="https://via.placeholder.com/720x841.png" alt="">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+</section>
 @endsection
