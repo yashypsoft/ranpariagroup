3074e564d7fc809af7495630efe45c688836a377
Yash Prajapati <yashprajapati@Yashs-MacBook-Air.local>
2022-09-10 15:04:25 +0530

    Image compress and added in folder

--- resources/views/corporate-video.blade.php
+++ resources/views/corporate-video.blade.php
@@ -7,8 +7,7 @@
     <br>
     <div class="container">
         <div class="video-container">
-            <iframe src="https://drive.google.com/file/d/1We2CQpBxe718hoPyc3R2tT8xvX5ycer1/preview" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            {{-- <iframe  src="https://www.youtube.com/embed/QI1n0niG5E4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> --}}
+            <iframe  src="https://www.youtube.com/embed/aga1jm8e8Gc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     </div>
 
